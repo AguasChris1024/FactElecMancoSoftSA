8632df3ffe59406ee5776183b693a3e1a459c855
Jessica Tituaña <jesifv1999@gmail.com>
2022-06-16 00:38:00 -0500

    CreacionEntidadCatalogoJE

---
+++ FacElectronica/src/main/java/FacturaElectronica/FacElectronicaApplication.java
@@ -0,0 +1,14 @@
+package FacturaElectronica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacElectronicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacElectronicaApplication.class, args);
+	}
+//Prueba Facturacion Electronica
+	
+}
