0c2a548652490e8128df67251b2309b64eda526f
Byron Morales <bmorales7119@uta.edu.ec>
2022-06-15 23:44:09 -0500

    CreacionInicialProyecto

---
+++ FacElectronica/src/main/java/com/example/demo/FacElectronicaApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacElectronicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacElectronicaApplication.class, args);
+	}
+//CREADO POR CHRISTIAN AGUAS 
+}
