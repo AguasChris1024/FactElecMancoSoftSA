106c49fcf5a03ccd20dc3e9336056f58847f2456
AguasChris1024 <59154503+AguasChris1024@users.noreply.github.com>
2022-07-11 17:02:48 -0500

    Creación de clase Detalle para la factura

--- src/main/java/pe/joedayz/clientes/entity/Detalle.java
+++ src/main/java/pe/joedayz/clientes/entity/Detalle.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 /**
  * Clase Detalle-Define los atributos detalle para la factura.
  * @author AG10
- *@version 2.0
+ *@version 3.0
  *@since 1.0
  */
 public class Detalle{
