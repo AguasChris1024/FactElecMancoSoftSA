9a4a14c0fdc745cefbb88e73fc9d7c9c2789cbdc
AguasChris1024 <59154503+AguasChris1024@users.noreply.github.com>
2022-07-11 17:09:33 -0500

    Correccion de atributo baseImponible dentro de la clase Impuesto

--- src/main/java/pe/joedayz/clientes/entity/Impuesto.java
+++ src/main/java/pe/joedayz/clientes/entity/Impuesto.java
@@ -9,7 +9,7 @@ package pe.joedayz.clientes.entity;
 public class Impuesto{
     public String codigo;
     public String codigoPorcentaje;
-    public int tarifa;
     public double baseImponible;
+    public int tarifa;
     public double valor;
 }
