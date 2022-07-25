bfe9415d1767dc9578f6461cc4def1c8c084ae56
Edward290394 <68308411+Edward290394@users.noreply.github.com>
2022-07-24 21:26:32 -0500

    [FAC-5] Implemetacion de cleinte repository

--- src/main/java/pe/joedayz/clientes/web/controller/JsController.java
+++ src/main/java/pe/joedayz/clientes/web/controller/JsController.java
@@ -1,8 +1,17 @@
 package pe.joedayz.clientes.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import pe.joedayz.clientes.repository.ClienteRepository1;
 
 @Controller
 public class JsController {
 
+	@Autowired
+	private ClienteRepository1 clienteRepository;
+	@Autowired
+	private ProductoRepository productoRepository;
+	
+	
 }
